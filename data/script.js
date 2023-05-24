d7d7771bd1b6319d93b5812bd2ae283e4ec66c21
hisazakura <91198475+hisazakura@users.noreply.github.com>
2023-05-24 08:39:54 +0700

    Updated UI
    UI still display only

--- data/script.js
+++ data/script.js
@@ -1,190 +1,70 @@
-// JS Scripts
-// Get current sensor readings when the page loads  
-window.addEventListener('load', getReadings);
+var mouse_on_humidity = false;
+var humidity_randomizer;
+var temperature_randomizer;
+var resistance_randomizer;
 
-// Create Temperature Gauge
-var gaugeTemp = new LinearGauge({
-  renderTo: 'gauge-temperature',
-  width: 120,
-  height: 400,
-  units: "Temperature C",
-  minValue: 0,
-  startAngle: 90,
-  ticksAngle: 180,
-  maxValue: 40,
-  colorValueBoxRect: "#049faa",
-  colorValueBoxRectEnd: "#049faa",
-  colorValueBoxBackground: "#e0e0e0",
-  valueDec: 2,
-  valueInt: 2,
-  majorTicks: [
-      "0",
-      "5",
-      "10",
-      "15",
-      "20",
-      "25",
-      "30",
-      "35",
-      "40"
-  ],
-  minorTicks: 4,
-  strokeTicks: true,
-  highlights: [
-      {
-          "from": 30,
-          "to": 40,
-          "color": "rgba(200, 50, 50, .75)"
-      }
-  ],
-  colorPlate: "#e0e0e0",
-  colorBarProgress: "#CC2936",
-  colorBarProgressEnd: "#049faa",
-  borderShadowWidth: 0,
-  borders: false,
-  needleType: "arrow",
-  needleWidth: 2,
-  needleCircleSize: 7,
-  needleCircleOuter: true,
-  needleCircleInner: false,
-  animationDuration: 500,
-  animationRule: "linear",
-  barWidth: 10,
-}).draw();
-  
-// Create Humidity Gauge
-var gaugeHum = new RadialGauge({
-  renderTo: 'gauge-humidity',
-  width: 300,
-  height: 300,
-  units: "Humidity (%)",
-  minValue: 0,
-  maxValue: 100,
-  colorValueBoxRect: "#049faa",
-  colorValueBoxRectEnd: "#049faa",
-  colorValueBoxBackground: "#e0e0e0",
-  valueInt: 2,
-  majorTicks: [
-      "0",
-      "20",
-      "40",
-      "60",
-      "80",
-      "100"
+window.addEventListener('load', (event) => {
+	updateHumidity();
+	updateTemperature();
+	updateResistance();
+});
 
-  ],
-  minorTicks: 4,
-  strokeTicks: true,
-  highlights: [
-      {
-          "from": 80,
-          "to": 100,
-          "color": "#03C0C1"
-      }
-  ],
-  colorPlate: "#e0e0e0",
-  borderShadowWidth: 0,
-  borders: false,
-  needleType: "line",
-  colorNeedle: "#007F80",
-  colorNeedleEnd: "#007F80",
-  needleWidth: 2,
-  needleCircleSize: 3,
-  colorNeedleCircleOuter: "#007F80",
-  needleCircleOuter: true,
-  needleCircleInner: false,
-  animationDuration: 500,
-  animationRule: "linear"
-}).draw();
-
-// Create Potensio Gauge
-var gaugePot = new RadialGauge({
-  renderTo: 'gauge-potensio',
-  width: 300,
-  height: 300,
-  units: "Potensio",
-  minValue: 0,
-  maxValue: 4095,
-  colorValueBoxRect: "#049faa",
-  colorValueBoxRectEnd: "#049faa",
-  colorValueBoxBackground: "#e0e0e0",
-  valueInt: 2,
-  majorTicks: [
-      "0",
-      "800",
-      "1600",
-      "2400",
-      "3200",
-      "4000"
-
-  ],
-  minorTicks: 4,
-  strokeTicks: true,
-  highlights: [
-      {
-          "from": 3270,
-          "to": 4095,
-          "color": "#03C0C1"
-      }
-  ],
-  colorPlate: "#e0e0e0",
-  borderShadowWidth: 0,
-  borders: false,
-  needleType: "line",
-  colorNeedle: "#007F80",
-  colorNeedleEnd: "#007F80",
-  needleWidth: 2,
-  needleCircleSize: 3,
-  colorNeedleCircleOuter: "#007F80",
-  needleCircleOuter: true,
-  needleCircleInner: false,
-  animationDuration: 1000,
-  animationRule: "linear"
-}).draw();
-
-// Function to get current readings on the webpage when it loads for the first time
-function getReadings(){
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var myObj = JSON.parse(this.responseText);
-      console.log(myObj);
-      var temp = myObj.temperature;
-      var hum = myObj.humidity;
-      var pot = myObj.potensio;
-      gaugeTemp.value = temp;
-      gaugeHum.value = hum;
-      gaugePot.value = pot;
-    }
-  }; 
-  xhr.open("GET", "/readings", true);
-  xhr.send();
+function setHumidity(val) {
+	document.documentElement.style.setProperty('--humidity', Math.min(Math.max(0, val), 1));
+	updateHumidity();
 }
 
-if (!!window.EventSource) {
-  var source = new EventSource('/events');
-  
-  source.addEventListener('open', function(e) {
-    console.log("Events Connected");
-  }, false);
+function setTemperature(val) {
+	document.documentElement.style.setProperty('--temperature', Math.min(Math.max(0, val), 1));
+	updateTemperature();
+}
 
-  source.addEventListener('error', function(e) {
-    if (e.target.readyState != EventSource.OPEN) {
-      console.log("Events Disconnected");
-    }
-  }, false);
-  
-  source.addEventListener('message', function(e) {
-    console.log("message", e.data);
-  }, false);
-  
-  source.addEventListener('new_readings', function(e) {
-    console.log("new_readings", e.data);
-    var myObj = JSON.parse(e.data);
-    console.log(myObj);
-    gaugeTemp.value = myObj.temperature;
-    gaugeHum.value = myObj.humidity;
-    gaugePot.value = myObj.potensio;
+function setResistance(val) {
+	document.documentElement.style.setProperty('--resistance', Math.min(Math.max(0, val), 1));
+	updateResistance();
+}
 
-  }, false);
+function randomizeAll(enabled) {
+	randomizeHumidity(enabled);
+	randomizeTemperature(enabled);
+	randomizeResistance(enabled);
+}
+
+function randomizeHumidity(enabled) {
+	if (enabled) humidity_randomizer = setInterval(() => {
+		setHumidity(Math.random());
+	}, 2000);
+	else clearInterval(humidity_randomizer);
+}
+
+function randomizeTemperature(enabled) {
+	if (enabled) temperature_randomizer = setInterval(() => {
+		setTemperature(Math.random());
+	}, 2000);
+	else clearInterval(temperature_randomizer);
+}
+
+function randomizeResistance(enabled) {
+	if (enabled) resistance_randomizer = setInterval(() => {
+		setResistance(Math.random());
+	}, 2000);
+	else clearInterval(resistance_randomizer);
+}
+
+function updateHumidity() {
+	var humidity = getComputedStyle(document.documentElement).getPropertyValue('--humidity');
+	var value = document.getElementById("humidity-value");
+	value.innerHTML = (humidity * 100).toFixed(0) + '%';
+}
+
+function updateTemperature() {
+	var temperature = getComputedStyle(document.documentElement).getPropertyValue('--temperature');
+	var value = document.getElementById("temperature-value");
+	value.innerHTML = (temperature * 100).toFixed(0) + '°';
+}
+
+function updateResistance() {
+	var resistance = getComputedStyle(document.documentElement).getPropertyValue('--resistance');
+	var value = document.getElementById("resistance-value");
+	value.innerHTML = (resistance * 10000).toFixed(0) + 'Ω';
 }
